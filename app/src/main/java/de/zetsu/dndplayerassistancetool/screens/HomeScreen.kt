5149b6d301da2d8102b33b456fb1db4dc653367f
Zetsuboushii <zetsuboushii@gmail.com>
2023-11-14 00:33:44 +0100

    feat 🦆 added next session card

--- app/src/main/java/de/zetsu/dndplayerassistancetool/screens/HomeScreen.kt
+++ app/src/main/java/de/zetsu/dndplayerassistancetool/screens/HomeScreen.kt
@@ -1,22 +1,26 @@
 package de.zetsu.dndplayerassistancetool.screens
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -31,22 +35,20 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import de.zetsu.dndplayerassistancetool.R
-import de.zetsu.dndplayerassistancetool.ui.theme.Purple40
-import java.time.Instant
+import java.text.SimpleDateFormat
+import java.util.Date
 
+@SuppressLint("SimpleDateFormat")
 @OptIn(ExperimentalMaterial3Api::class)
 @Preview
 @Composable
 fun Home() {
-    val datePickerState = rememberDatePickerState(
-        initialSelectedDateMillis = Instant.now().toEpochMilli()
-    )
-
     Column {
-        Row(
+        // Title + Logo
+        Column(
             modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.Center,
-            verticalAlignment = Alignment.CenterVertically
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
         ) {
             Text(
                 text = "D&D Player Assistance Tool",
@@ -54,21 +56,17 @@ fun Home() {
                 fontWeight = FontWeight.Bold,
                 modifier = Modifier.padding(5.dp)
             )
-        }
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.Center,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
             Image(
                 painter = painterResource(id = R.drawable.dnd_logo_transparent),
                 contentDescription = null,
                 modifier = Modifier
                     .size(200.dp)
-                    .padding(5.dp)
             )
         }
-        Spacer(modifier = Modifier.size(40.dp))
+
+        Spacer(modifier = Modifier.size(100.dp))
+
+        // Search Bar
         Row(
             modifier = Modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.Center,
@@ -85,19 +83,48 @@ fun Home() {
             )
 
         }
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.Center,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Column(
-                modifier = Modifier
-                    .padding(40.dp)
-                    .background(Purple40)
-            ) {
 
+        Spacer(modifier = Modifier.size(20.dp))
+
+        // Next Session Card
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            ElevatedCard(
+                colors = CardDefaults.cardColors(
+                    containerColor = MaterialTheme.colorScheme.surfaceVariant
+                ),
+                modifier = Modifier
+                    .width(300.dp)
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(20.dp)
+                ) {
+                    Text(
+                        text = "Next Session on",
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.Bold,
+                    )
+                }
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(20.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center,
+                ) {
+                    Text(
+                        text = SimpleDateFormat("dd.MM.yyyy").format(Date()),
+                        fontSize = 30.sp
+                    )
+                }
             }
         }
 
     }
+
 }
