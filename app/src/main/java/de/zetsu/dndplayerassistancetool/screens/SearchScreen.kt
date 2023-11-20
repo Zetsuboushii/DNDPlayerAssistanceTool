fc4b1c13ea47eb6d861235351a21d7429549fc6a
Zetsuboushii <zetsuboushii@gmail.com>
2023-11-20 17:36:29 +0100

    feat 🦆 minor changes

--- app/src/main/java/de/zetsu/dndplayerassistancetool/screens/SearchScreen.kt
+++ app/src/main/java/de/zetsu/dndplayerassistancetool/screens/SearchScreen.kt
@@ -1,7 +1,6 @@
 package de.zetsu.dndplayerassistancetool.screens
 
 import android.content.Context
-import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -23,7 +22,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -32,22 +30,16 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleEventObserver
-import androidx.lifecycle.LifecycleOwner
 import de.zetsu.dndplayerassistancetool.R
-import de.zetsu.dndplayerassistancetool.SpellProvider
 import de.zetsu.dndplayerassistancetool.dataclasses.AreaOfEffect
 import de.zetsu.dndplayerassistancetool.dataclasses.AreaOfEffectType
 import de.zetsu.dndplayerassistancetool.dataclasses.Class
 import de.zetsu.dndplayerassistancetool.dataclasses.Damage
 import de.zetsu.dndplayerassistancetool.dataclasses.DamageType
 import de.zetsu.dndplayerassistancetool.dataclasses.School
-import de.zetsu.dndplayerassistancetool.dataclasses.Spell
 import de.zetsu.dndplayerassistancetool.dataclasses.SpellDetail
 import kotlinx.coroutines.launch
 
@@ -123,7 +115,7 @@ fun Search(context: Context) {
                 horizontalArrangement = Arrangement.Center,
                 verticalAlignment = Alignment.CenterVertically
             ) {
-                var text by remember {mutableStateOf(TextFieldValue(""))}
+                var text by remember { mutableStateOf(TextFieldValue("")) }
                 OutlinedTextField(
                     value = text,
                     onValueChange = { text = it },
