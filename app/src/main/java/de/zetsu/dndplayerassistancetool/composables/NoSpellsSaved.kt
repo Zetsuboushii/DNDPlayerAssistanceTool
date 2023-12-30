439e284ccb1707ef9524ec0a5072b5fb4d585e14
isa <idabarbu.ib@gmail.com>
2023-12-30 16:24:34 +0100

    feature  dark mode adaptation

--- app/src/main/java/de/zetsu/dndplayerassistancetool/composables/NoSpellsSaved.kt
+++ app/src/main/java/de/zetsu/dndplayerassistancetool/composables/NoSpellsSaved.kt
@@ -13,6 +13,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.material.icons.outlined.Warning
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -35,110 +36,112 @@ import androidx.compose.ui.unit.sp
 
 @Composable
 fun NoSpellsSaved() {
-        LazyColumn(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp)
-        ) {
-            item {
-                Card(
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+    ) {
+        item {
+            Card(
+
+                colors = CardDefaults.cardColors(
+                    containerColor = MaterialTheme.colorScheme.surfaceVariant
+                ),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(25.dp),
+                shape = MaterialTheme.shapes.medium
+            ) {
+                Row(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(25.dp),
-                    shape = MaterialTheme.shapes.medium
+                        .padding(16.dp),
+                    verticalAlignment = Alignment.CenterVertically
                 ) {
-                    Row(
+                    Icon(
+                        imageVector = Icons.Outlined.Warning,
+                        contentDescription = null,
                         modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(16.dp),
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        Icon(
-                            imageVector = Icons.Outlined.Warning,
-                            contentDescription = null,
-                            modifier = Modifier
-                                .size(30.dp)
-                                .padding(4.dp)
-                                .offset(0.dp, 1.dp),
-                            tint = Color.DarkGray
-                        )
+                            .size(30.dp)
+                            .padding(4.dp)
+                            .offset(0.dp, 1.dp),
+                        //tint = if (MaterialTheme.colorScheme.isDark) Color.White else Color.DarkGray
+                    )
 
-                        Spacer(modifier = Modifier.width(20.dp))
-                        Text(
-                            text = "You have not saved \nany spells yet...",
-                            color = Color(0xFF5C07C8),
-                            fontFamily = FontFamily.Serif,
-                            fontWeight = FontWeight.Normal,
-                            fontSize = 20.sp,
-                        )
-                    }
-                }
-        }
-
-            item {
-                var expanded by remember { mutableStateOf(false) }
-
-                Card(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(25.dp)
-                        .clickable { expanded = !expanded },
-                    shape = MaterialTheme.shapes.medium
-                ) {
-                    Row(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(16.dp),
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        Icon(
-                            imageVector = Icons.Outlined.Settings,
-                            contentDescription = null,
-                            modifier = Modifier
-                                .size(30.dp)
-                                .padding(4.dp)
-                                .offset(0.dp, 1.dp),
-                            tint = Color.DarkGray
-                        )
-
-                        Spacer(modifier = Modifier.width(20.dp))
-                        Text(
-                            text = "Don't know how to add a Spell? --> Tap Me",
-                            color = Color(0xFF5C07C8),
-                            fontFamily = FontFamily.Serif,
-                            fontWeight = FontWeight.Normal,
-                            fontSize = 20.sp,
-                        )
-                    }
-                    if (expanded) {
-                        Text(
-                            text = buildAnnotatedString {
-                                withStyle(
-                                    style = SpanStyle(
-                                        fontSize = 18.sp,
-                                        fontFamily = FontFamily.Serif,
-                                        fontWeight = FontWeight.W400,
-                                        color = Color.DarkGray
-                                    )
-                                )
-
-                                {
-                                    Divider(Modifier.padding(10.dp), thickness = 2.dp)
-                                    appendLine("1. For starters:\n ")
-                                    appendLine("I contain a description when I expand, like all our spells.\n")
-                                    appendLine("2. To save a spell:\n")
-                                    appendLine("Go to the Spell Search Button on the bottom.")
-                                    appendLine("With a long tap on a spell (hold 2 sec.), you save it.\n")
-                                    appendLine("3. To delete a spell:\n")
-                                    appendLine("With a long tap in the Spell Book, you delete the spell.")
-                                }
-                            },
-                            modifier = Modifier.padding(14.dp)
-                        )
-                    }
+                    Spacer(modifier = Modifier.width(20.dp))
+                    Text(
+                        text = "You have not saved \nany spells yet...",
+                        color = Color(0xFFDE281D),
+                        fontFamily = FontFamily.Serif,
+                        fontWeight = FontWeight.Normal,
+                        fontSize = 20.sp,
+                    )
                 }
             }
         }
 
+        item {
+            var expanded by remember { mutableStateOf(false) }
 
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(25.dp)
+                    .clickable { expanded = !expanded },
+                shape = MaterialTheme.shapes.medium
+            ) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(
+                        imageVector = Icons.Outlined.Settings,
+                        contentDescription = null,
+                        modifier = Modifier
+                            .size(30.dp)
+                            .padding(4.dp)
+                            .offset(0.dp, 1.dp),
+                        //tint = if (MaterialTheme.colorScheme.isDark) Color.White else Color.DarkGray
+                    )
+
+                    Spacer(modifier = Modifier.width(20.dp))
+                    Text(
+                        text = "Don't know how to add a Spell? --> Tap Me",
+                        color = Color(0xFFDE281D),
+                        fontFamily = FontFamily.Serif,
+                        fontWeight = FontWeight.Normal,
+                        fontSize = 20.sp,
+                    )
+                }
+                if (expanded) {
+                    Text(
+                        text = buildAnnotatedString {
+                            withStyle(
+                                style = SpanStyle(
+                                    fontSize = 18.sp,
+                                    fontFamily = FontFamily.Serif,
+                                    fontWeight = FontWeight.W400,
+                                    //color = if (MaterialTheme.colorScheme.isDark) Color.White else Color.DarkGray
+                                )
+                            )
+
+                            {
+                                Divider(Modifier.padding(10.dp), thickness = 2.dp)
+                                appendLine("1. For starters:\n ")
+                                appendLine("I contain a description when I expand, like all our spells.\n")
+                                appendLine("2. To save a spell:\n")
+                                appendLine("Go to the Spell Search Button on the bottom.")
+                                appendLine("With a long tap on a spell (hold 2 sec.), you save it.\n")
+                                appendLine("3. To delete a spell:\n")
+                                appendLine("With a long tap in the Spell Book, you delete the spell.")
+                            }
+                        },
+                        modifier = Modifier.padding(14.dp)
+                    )
+                }
+            }
+        }
+    }
 }
